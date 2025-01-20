4246963a59b126dd0cb4f6bc2bef54c58e148eec
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-01-20 22:13:03 +0300

    Add project files.

---
+++ OnlineTicariOtomasyon/Models/Siniflar/Kategori.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Kategori
+    {
+        [Key]
+        public int Kategoriid { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string KategorAd { get; set; }
+        public ICollection<Urun>Uruns { get; set; }
+    }
+}
