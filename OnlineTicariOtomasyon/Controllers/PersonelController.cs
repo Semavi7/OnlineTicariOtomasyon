68f03da546b334805f0809fdb61f6dfe4fc10883
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-01-24 00:30:44 +0300

    OnlineTicariOtomasyon

---
+++ OnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Siniflar;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        Context c = new Context();
+        // GET: Personel
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.Where(x => x.Durum == true).ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.Departmanid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel p)
+        {
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.Where(x => x.Durum == true).ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.Departmanid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var prs = c.Personels.Find(id);
+            return View("PersonelGetir", prs);
+        }
+
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var prsn = c.Personels.Find(p.Personelid);
+            prsn.PersonelAd = p.PersonelAd;
+            prsn.PersonelSoyad = p.PersonelSoyad;
+            prsn.PersonelGörsel = p.PersonelGörsel;
+            prsn.DepartmanId = p.DepartmanId;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- OnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
+++ OnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -15,14 +15,16 @@ namespace OnlineTicariOtomasyon.Models.Siniflar
         [StringLength(30)]
         public string CariAd { get; set; }
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30, ErrorMessage = "En Fazla 30 Karakter Yazabilirsiniz!")]
         public string CariSoyad { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(13)]
+        [Required(ErrorMessage ="Bu Alanı Boş Geçemezsiniz!")]
         public string CariSehir { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string CariMail { get; set; }
+        public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
 }
