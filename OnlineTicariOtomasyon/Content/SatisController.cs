f6327fcf094ea42a2bc4a4c1ea434d21018c6cf9
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-01-25 00:20:53 +0300

    OnlineTicariOtomasyon

---
+++ OnlineTicariOtomasyon/Content/SatisController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Siniflar;
+
+namespace OnlineTicariOtomasyon.Content
+{
+    public class SatisController : Controller
+    { 
+        Context c = new Context();
+        // GET: Satis
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList() 
+                                           select new SelectListItem 
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.Urunid.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.Cariid.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s)
+        {
+            s.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SatisGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.Urunid.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.Cariid.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            var deger = c.SatisHarekets.Find(id);
+            return View("SatisGetir", deger);
+        }
+
+        public ActionResult SatisGüncelle(SatisHareket s)
+        {
+            var degerler = c.SatisHarekets.Find(s.Satisid);
+            degerler.CariId = s.CariId;
+            degerler.Adet = s.Adet;
+            degerler.PersonelId = s.PersonelId;
+            degerler.Tarih = s.Tarih;
+            degerler.Fiyat = s.Fiyat;
+            degerler.ToplamTutar = s.ToplamTutar;
+            degerler.UrunId = s.UrunId;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SatisDetay(int id)
+        {
+            var degerler = c.SatisHarekets.Where(x => x.Satisid == id).ToList();
+            return View(degerler);
+        }
+    }
+}
--- OnlineTicariOtomasyon/Controllers/UrunController.cs
+++ OnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -74,5 +74,11 @@ namespace OnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult UrunListesi()
+        {
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
+        }
     }
 }
