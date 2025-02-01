79d8a62bf6b3ddef8dd69f825a3dba3efecfc888
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-02-01 20:44:42 +0300

    OnlineTicariOtomasyon

---
+++ OnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Siniflar;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        Context c = new Context();
+        // GET: CariPanel
+        [Authorize]
+        public ActionResult Index()
+        {
+            var mail = (string)Session["CariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            return View(degerler);
+        }
+
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.Cariid).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.CariId == id).ToList();
+            return View(degerler);
+        }
+    }
+}
