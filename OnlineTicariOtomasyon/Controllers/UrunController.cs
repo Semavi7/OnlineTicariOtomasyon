07557bdcb3b7aab3694f3375c6954e5f89295d73
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-02-05 23:10:08 +0300

    OnlineTicariOtomasyon

--- OnlineTicariOtomasyon/Controllers/UrunController.cs
+++ OnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -86,5 +86,35 @@ namespace OnlineTicariOtomasyon.Controllers
             var degerler = c.Uruns.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult SatisYap(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger4 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.Cariid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var deger2 = c.Uruns.Find(id);
+            ViewBag.dgr2 = deger2.Urunid;
+            ViewBag.dgr3 = deger2.SatisFiyat;
+            ViewBag.dgr4 = deger4;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SatisYap(SatisHareket p)
+        {
+            p.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index", "Satis");
+        }
     }
 }
