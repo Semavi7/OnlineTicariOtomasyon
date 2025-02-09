ca31249b74a7cf35d240bd968e46b66d50c2c9d3
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-02-09 21:30:33 +0300

    OnlineTicariOtomasyon

--- OnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ OnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OnlineTicariOtomasyon.Models.Siniflar;
+using QRCoder;
+using System.Web.Security;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
@@ -20,6 +25,7 @@ namespace OnlineTicariOtomasyon.Controllers
             return View(degerler);
         }
 
+        [Authorize]
         public ActionResult Siparislerim()
         {
             var mail = (string)Session["CariMail"];
@@ -28,6 +34,7 @@ namespace OnlineTicariOtomasyon.Controllers
             return View(degerler);
         }
 
+        [Authorize]
         public ActionResult GelenMesajlar()
         {
             var mail = (string)Session["CariMail"];
@@ -39,6 +46,7 @@ namespace OnlineTicariOtomasyon.Controllers
             return View(mesajlar);
         }
 
+        [Authorize]
         public ActionResult GidenMesajlar()
         {
             var mail = (string)Session["CariMail"];
@@ -50,6 +58,7 @@ namespace OnlineTicariOtomasyon.Controllers
             return View(mesajlar);
         }
 
+        [Authorize]
         public ActionResult MesajDetay(int id)
         {
             var degerler = c.Mesajlars.Where(x => x.MesajId == id).ToList();
@@ -61,6 +70,7 @@ namespace OnlineTicariOtomasyon.Controllers
             return View(degerler);
         }
 
+        [Authorize]
         [HttpGet]
         public ActionResult YeniMesaj()
         {
@@ -71,6 +81,8 @@ namespace OnlineTicariOtomasyon.Controllers
             ViewBag.d2 = gidensayisi;
             return View();
         }
+
+        [Authorize]
         [HttpPost]
         public ActionResult YeniMesaj(Mesajlar m)
         {
@@ -80,6 +92,44 @@ namespace OnlineTicariOtomasyon.Controllers
             c.Mesajlars.Add(m);
             c.SaveChanges();
             return RedirectToAction("GelenMesajlar");
+        }
+
+        [Authorize]
+        public ActionResult KargoTakip(string p)
+        {
+            var k = from x in c.KargoDetays select x;
+            k = k.Where(y => y.TakipKodu.Contains(p));
+            return View(k.ToList());
+        }
+
+        [Authorize]
+        public ActionResult CariKargoTakip(string id)
+        {
+            var degerler = c.KargoTakips.Where(x => x.TakipKodu == id).ToList();
+            return View(degerler);
+        }
+
+        [Authorize]
+        public ActionResult CariKarekod(int id)
+        {
+            var takip = c.KargoDetays.Find(id);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator koduret = new QRCodeGenerator();
+                QRCodeGenerator.QRCode karekod = koduret.CreateQrCode(takip.TakipKodu, QRCodeGenerator.ECCLevel.Q);
+                using (Bitmap resim = karekod.GetGraphic(10))
+                {
+                    resim.Save(ms, ImageFormat.Png);
+                    ViewBag.karekodimage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return View("CariKarekod", takip);
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
